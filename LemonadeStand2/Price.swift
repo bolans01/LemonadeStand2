442e7161d4ec0b58fc9004ff1b713af408f92151
bolans01 <bolans01@gmail.com>
2015-09-06 21:53:12 -0400

    First commit

---
+++ LemonadeStand2/Price.swift
@@ -0,0 +1,15 @@
+//
+//  Price.swift
+//  LemonadeStand2
+//
+//  Created by Neel Bola on 7/14/15.
+//  Copyright (c) 2015 Neel Bola. All rights reserved.
+//
+
+import Foundation
+
+struct Price {
+    let lemon = 2
+    let iceCube = 1
+    
+}
---
+++ LemonadeStand2/Supplies.swift
@@ -0,0 +1,25 @@
+//
+//  Supplies.swift
+//  LemonadeStand2
+//
+//  Created by Neel Bola on 7/14/15.
+//  Copyright (c) 2015 Neel Bola. All rights reserved.
+//
+
+import Foundation
+
+
+struct Supplies {
+    var money = 0
+    var lemons = 0
+    var iceCubes = 0
+    
+    init (aMoney: Int, aLemons: Int, aIceCubes: Int) {
+        money = aMoney
+        lemons = aLemons
+        iceCubes = aIceCubes
+    }
+}
+
+
+
